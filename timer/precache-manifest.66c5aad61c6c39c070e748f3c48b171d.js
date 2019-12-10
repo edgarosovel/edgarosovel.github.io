6fbaaadb7e7144979b35d0cdf424e47930d43e3d
edgarosovel <edgarov@live.com.mx>
2019-12-09 21:16:18 -0600

    adding /timer

---
+++ timer/precache-manifest.66c5aad61c6c39c070e748f3c48b171d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb89c1fbd9765900f8b97ce858ce31ff",
+    "url": "/timer/index.html"
+  },
+  {
+    "revision": "a6bf88df221112ba2917",
+    "url": "/timer/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "f13f20c33434d3f7776a",
+    "url": "/timer/static/css/main.82f96c8d.chunk.css"
+  },
+  {
+    "revision": "a6bf88df221112ba2917",
+    "url": "/timer/static/js/2.8781de45.chunk.js"
+  },
+  {
+    "revision": "b08610a15aea65eb1a2f6008ea11a714",
+    "url": "/timer/static/js/2.8781de45.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f13f20c33434d3f7776a",
+    "url": "/timer/static/js/main.a4a7b916.chunk.js"
+  },
+  {
+    "revision": "9d039d8f8e94d72d36c6",
+    "url": "/timer/static/js/runtime-main.426b52d6.js"
+  },
+  {
+    "revision": "d5acce1f8321ce64701b0ac52f5c788e",
+    "url": "/timer/static/media/alarm.d5acce1f.mp3"
+  }
+]);
