484f34128c891927e2526d03d3c89b0f36937d9c
edgarosovel <edgarov@live.com.mx>
2019-12-29 23:23:22 -0600

    Added new pass gen

---
+++ pass/precache-manifest.c7d29777aa0dd85427b3062f25b787ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "584691d3a17025754c15bd4f364fe6bf",
+    "url": "/pass/index.html"
+  },
+  {
+    "revision": "27f36ed839b0ec4b3233",
+    "url": "/pass/static/css/main.4c70dbdd.chunk.css"
+  },
+  {
+    "revision": "91a52df066673642334c",
+    "url": "/pass/static/js/2.5b6a5b87.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pass/static/js/2.5b6a5b87.chunk.js.LICENSE"
+  },
+  {
+    "revision": "27f36ed839b0ec4b3233",
+    "url": "/pass/static/js/main.760ae3a2.chunk.js"
+  },
+  {
+    "revision": "d9ac3a8e08b5ca6b9c18",
+    "url": "/pass/static/js/runtime-main.8e51aeb2.js"
+  }
+]);
