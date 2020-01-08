a21462dc65de8a8ba6fa9b7a4287dba3b04c9183
edgarosovel <edgarov@live.com.mx>
2020-01-07 19:14:35 -0600

    Huckleberry

---
+++ huckleberry/precache-manifest.8d4fac70af284717c0cbe98a50732e82.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10641181cb17da3c5eed1a9e46da4df5",
+    "url": "/huckleberry/index.html"
+  },
+  {
+    "revision": "a4479be6ab8df87d6485",
+    "url": "/huckleberry/static/css/main.ee476be3.chunk.css"
+  },
+  {
+    "revision": "287632d92fa2ea4921fb",
+    "url": "/huckleberry/static/js/2.3bf7262a.chunk.js"
+  },
+  {
+    "revision": "a4479be6ab8df87d6485",
+    "url": "/huckleberry/static/js/main.357000e2.chunk.js"
+  },
+  {
+    "revision": "141b82203390f58b8c00",
+    "url": "/huckleberry/static/js/runtime-main.bff2ff98.js"
+  },
+  {
+    "revision": "c47d7e581f2a563f8dedd4509c106fb8",
+    "url": "/huckleberry/static/media/Circular.c47d7e58.woff"
+  },
+  {
+    "revision": "b5204529b5b0b1cbe2831de7edbed75f",
+    "url": "/huckleberry/static/media/Playfair_display.b5204529.woff2"
+  },
+  {
+    "revision": "02d44fa891eb7b5f6ec5af191b162ee6",
+    "url": "/huckleberry/static/media/house.02d44fa8.png"
+  },
+  {
+    "revision": "a5821669d9013307088c65d77e45e396",
+    "url": "/huckleberry/static/media/user.a5821669.jpg"
+  }
+]);
